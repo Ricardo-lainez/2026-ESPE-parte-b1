8a0433835de3f6c0f019e90592fd97cd0d68e586
Ricardo Andres Laínez Suárez <ricardoandreslainezsuarez@gmail.com>
2026-01-22 08:29:54 -0500

    pruebas de unidad jUnit

--- src/test/java/es/upm/grise/profundizacion/subscriptionService/SubscriptionServiceTest.java
+++ src/test/java/es/upm/grise/profundizacion/subscriptionService/SubscriptionServiceTest.java
@@ -1,10 +1,62 @@
 package es.upm.grise.profundizacion.subscriptionService;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import es.upm.grise.profundizacion.exceptions.ExistingUserException;
+import es.upm.grise.profundizacion.exceptions.LocalUserDoesNotHaveNullEmailException;
+import es.upm.grise.profundizacion.exceptions.NullUserException;
 
 public class SubscriptionServiceTest {
 	
-	@Test
-	public void smokeTest() {}
+	private SubscriptionService subscriptionService;
+	private User user;
 	
+	@BeforeEach
+	public void setUp() {
+		subscriptionService = new SubscriptionService();
+		user = new User();
+	}
+	
+	@Test
+	public void testAddSubscriberWithNullUserThrowsException() {
+		assertThrows(NullUserException.class, () -> {
+			subscriptionService.addSubscriber(null);
+		});
+	}
+	
+	@Test
+	public void testAddSubscriberWithValidUser() throws NullUserException, ExistingUserException, LocalUserDoesNotHaveNullEmailException {
+		user.setEmail("ricardo@espe.com");
+		user.setDelivery(Delivery.DO_NOT_DELIVER);
+		
+		subscriptionService.addSubscriber(user);
+		
+		assertTrue(subscriptionService.getSubscribers().contains(user));
+		assertEquals(1, subscriptionService.getSubscribers().size());
+	}
+	
+	@Test
+	public void testAddExistingUserThrowsException() throws NullUserException, ExistingUserException, LocalUserDoesNotHaveNullEmailException {
+		user.setEmail("ricardo@espe.com");
+		user.setDelivery(Delivery.DO_NOT_DELIVER);
+		
+		subscriptionService.addSubscriber(user);
+		
+		assertThrows(ExistingUserException.class, () -> {
+			subscriptionService.addSubscriber(user);
+		});
+	}
+	
+	@Test
+	public void testAddLocalUserWithEmailThrowsException() {
+		user.setEmail("ricardo@espe.com");
+		user.setDelivery(Delivery.LOCAL);
+		
+		assertThrows(LocalUserDoesNotHaveNullEmailException.class, () -> {
+			subscriptionService.addSubscriber(user);
+		});
+	}
 }
